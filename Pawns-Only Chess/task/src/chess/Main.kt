6a0b53148bc8f585adaa89736de74396e29caa9b
Irina Keil <35578733+Luftschutzkeller@users.noreply.github.com>
2021-11-25 21:44:41 +0300

    Stage 2

--- Pawns-Only Chess/task/src/chess/Main.kt
+++ Pawns-Only Chess/task/src/chess/Main.kt
@@ -1,30 +1,47 @@
 package chess
 
 fun main() {
-    println("Pawns-Only Chess")
-    println(board())
-}
+    println("Pawns-Only Chess\n" +
+            "First Player's name:")
 
-fun board(): String {
-    val empty = " "
-    val black = "B"
-    val white = "W"
-    var board: String = ""
-    val line = "  +---+---+---+---+---+---+---+---+\n"
-    for (i in 8 downTo 1) {
-        board += line
-        board += "$i |"
-        for (j in 1..8) {
-            board += when (i) {
-                7 -> " $black |"
-                2 -> " $white |"
-                else -> " $empty |"
-            }
-        }
-        board += "\n"
+    val name1 = readLine()!!
+
+    println("Second Player's name:")
+
+    val name2 = readLine()!!
+
+    println("  +---+---+---+---+---+---+---+---+\n" +
+            "8 |   |   |   |   |   |   |   |   |\n" +
+            "  +---+---+---+---+---+---+---+---+\n" +
+            "7 | B | B | B | B | B | B | B | B |\n" +
+            "  +---+---+---+---+---+---+---+---+\n" +
+            "6 |   |   |   |   |   |   |   |   |\n" +
+            "  +---+---+---+---+---+---+---+---+\n" +
+            "5 |   |   |   |   |   |   |   |   |\n" +
+            "  +---+---+---+---+---+---+---+---+\n" +
+            "4 |   |   |   |   |   |   |   |   |\n" +
+            "  +---+---+---+---+---+---+---+---+\n" +
+            "3 |   |   |   |   |   |   |   |   |\n" +
+            "  +---+---+---+---+---+---+---+---+\n" +
+            "2 | W | W | W | W | W | W | W | W |\n" +
+            "  +---+---+---+---+---+---+---+---+\n" +
+            "1 |   |   |   |   |   |   |   |   |\n" +
+            "  +---+---+---+---+---+---+---+---+\n" +
+            "    a   b   c   d   e   f   g   h\n")
+
+    var turn = name1
+
+    while (true) {
+        println("$turn's turn:")
+
+        val input = readLine()!!
+
+        if (input == "exit") break
+
+        if (input.isValidMove()) turn = if (turn == name1) name2 else name1 else println("Invalid Input")
     }
-    board += line
-    board += "    a   b   c   d   e   f   g   h"
 
-    return board
+    println("Bye!")
 }
+
+fun String.isValidMove(): Boolean = Regex("[a-h][1-8][a-h][1-8]").matches(this)

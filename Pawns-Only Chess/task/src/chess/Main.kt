c50db6584799e619946c4623065f2294201cac41
Irina Keil <35578733+Luftschutzkeller@users.noreply.github.com>
2021-11-26 15:44:04 +0300

    Stage 4

--- Pawns-Only Chess/task/src/chess/Main.kt
+++ Pawns-Only Chess/task/src/chess/Main.kt
@@ -1,4 +1,9 @@
 package chess
+import kotlin.math.abs
+
+val board = Array(8) { Array(8) { ' ' } }
+val whitePawns = Array(8) { Pawn("white", it, 6) }
+val blackPawns = Array(8) { Pawn("black", it, 1) }
 
 fun main() {
     println("Pawns-Only Chess\n" +
@@ -10,20 +15,20 @@ fun main() {
 
     val name2 = readLine()!!
     var turn = name1
-    val board = Array(8) { Array(8) { ' ' } }
 
-    for (i in 0..7) board[6][i] = 'W'
-    for (i in 0..7) board[1][i] = 'B'
+    whitePawns.forEach { board[it.y][it.x] = it.color.first().uppercaseChar() }
+    blackPawns.forEach { board[it.y][it.x] = it.color.first().uppercaseChar() }
 
-    board.print()
+    printBoard()
 
     while (true) {
         println("$turn's turn:")
 
         val input = readLine()!!
-        val color = if (turn == name1) "white" else "black"
 
         if (input == "exit") break
+
+        val color = if (turn == name1) "white" else "black"
 
         if (!input.isValidInput()) println("Invalid Input") else {
             val x1 = input[0].code - 97
@@ -32,10 +37,20 @@ fun main() {
             val y2 = 8 - input[3].digitToInt()
 
             if (board[y1][x1] != color[0].uppercaseChar()) println("No $color pawn at ${input.substring(0..1)}") else {
-                if (board.isValidMove(x1, y1, x2, y2, color)) {
+                if (isValidMove(x1, y1, x2, y2, color)) {
                     board[y1][x1] = ' '
                     board[y2][x2] = color[0].uppercaseChar()
-                    board.print()
+
+                    getPawnAt(x1, y1).x = x2
+                    getPawnAt(x2, y1).y = y2
+
+                    printBoard()
+
+                    if (color == "white") {
+                        blackPawns.forEach { if (it.y != 1) it.firstMove = false}
+                    } else {
+                        whitePawns.forEach { if (it.y != 6) it.firstMove = false}
+                    }
 
                     turn = if (turn == name1) name2 else name1
                 } else println("Invalid Input")
@@ -46,10 +61,19 @@ fun main() {
     println("Bye!")
 }
 
-fun Array<Array<Char>>.print() {
+class Pawn(val color: String, var x: Int, var y: Int, var captured: Boolean = false, var firstMove: Boolean = true)
+
+fun getPawnAt(x: Int, y: Int): Pawn {
+    whitePawns.forEach { if (it.x == x && it.y == y) return it }
+    blackPawns.forEach { if (it.x == x && it.y == y) return it }
+
+    return Pawn("none", -1, -1)
+}
+
+fun printBoard() {
     println("  +---+---+---+---+---+---+---+---+")
 
-    this.forEachIndexed { i, row ->
+    board.forEachIndexed { i, row ->
         print("${8 - i} ")
 
         row.forEach { print("| $it ") }
@@ -61,18 +85,35 @@ fun Array<Array<Char>>.print() {
     println("    a   b   c   d   e   f   g   h\n")
 }
 
-fun Array<Array<Char>>.isValidMove(x1: Int, y1: Int, x2: Int, y2: Int, color: String): Boolean {
-    val oppositeColor = if (color == "white") "black" else "white"
-
-    if (x1 != x2 || this[y2][x2] == oppositeColor[0].uppercaseChar()) return false
-
-    val firstMove = color == "white" && y1 == 6 || color == "black" && y1 == 1
-
-    return if (color == "white") {
-        y1 - y2 == 1 || firstMove && y1 - y2 == 2
-    } else {
-        y2 - y1 == 1 || firstMove && y2 - y1 == 2
-    }
-}
-
 fun String.isValidInput(): Boolean = Regex("[a-h][1-8][a-h][1-8]").matches(this)
+
+fun isValidMove(x1: Int, y1: Int, x2: Int, y2: Int, turn: String): Boolean {
+    val oppositeColor = if (turn == "white") "black" else "white"
+    val yC1 = if (turn == "white") y1 else y2
+    val yC2 = if (turn == "white") y2 else y1
+    val enPassantRow = if (turn == "white") 3 else 4
+
+    if (x1 == x2) {
+        if (board[y2][x2] == oppositeColor[0].uppercaseChar()) return false
+
+        return yC1 - yC2 == 1 || getPawnAt(x1, y1).firstMove && yC1 - yC2 == 2
+    } else {
+        if (yC1 - yC2 != 1 || abs(x1 - x2) != 1) return false
+
+        if (getPawnAt(x2, y2).color == oppositeColor) {
+            getPawnAt(x2, y2).captured = true
+
+            return true
+        }
+
+        if (y1 == enPassantRow && getPawnAt(x2, y1).color == oppositeColor && getPawnAt(x2, y1).firstMove) {
+            getPawnAt(x2, y1).captured = true
+
+            board[y1][x2] = ' '
+
+            return true
+        }
+    }
+
+    return false
+}
